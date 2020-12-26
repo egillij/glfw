190a15d50e186cae0efddf80cb387edc0974c44f
= <=>
2020-12-26 21:48:34 +0100

    premake build fixed+

--- premake5.lua
+++ premake5.lua
@@ -25,20 +25,20 @@ project "GLFW"
 		
 		files
 		{
-		"src/win32_platform.h",
-		"src/win32_joystick.h",
-		"src/wgl_context.h",
-		"src/egl_context.h",
-		"src/osmesa_context.h",
-		"src/win32_init.c"
-		"src/win32_joystick.c",
-		"src/win32_monitor.c",
-		"src/win32_time.c",
-		"src/win32_thread.c",
-		"src/win32_window.c",
-		"src/wgl_context.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c"
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
+			"src/wgl_context.h",
+			"src/egl_context.h",
+			"src/osmesa_context.h",
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
 		}
 		
 		defines
